7e78b1378000dcd6aec18a4cf5d068e997b45c91
kervisvasquez1993 <kervisvasquez24@gmail.com>
2026-06-17 09:27:43 -0300

    feat: initial project setup

---
+++ commitlint.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 100],
+    'body-max-line-length': [2, 'always', 100],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'refactor',
+        'perf',
+        'test',
+        'docs',
+        'chore',
+        'build',
+        'ci',
+        'style',
+        'revert',
+      ],
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+  },
+}
